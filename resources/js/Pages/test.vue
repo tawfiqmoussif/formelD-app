f957a4e357d5a15eb634d6d16fccb53c47275fbd
tawfiqmoussif <tawfiqmoussif@gmail.com>
2021-09-13 17:59:50 +0100

    add vuetify and jetstream successfully

---
+++ resources/js/Pages/test.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-app>
+        <v-container grid-list-xs>
+            <v-btn color="success">test</v-btn>
+        </v-container>
+    </v-app>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
@@ -21,5 +22,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
